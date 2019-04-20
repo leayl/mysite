74f3d156802ec8b8b9cda5310c9aaa7874791840
leayl <lea_yl@163.com>
2019-04-20 23:28:32 +0800

    optimize website with bootstrap

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,8 @@ from blog.models import Blog, BlogType
 def blog_list(request):
     context={}
     context["blogs"]=Blog.objects.all()
+    context["blog_types"]=BlogType.objects.all()
+
     return render(request,"blog/blog_list.html",context)
 
 def blog_detail(request,blog_pk):
@@ -18,4 +20,5 @@ def blogs_with_type(request,blog_type_pk):
     context={}
     context["blogs"]=Blog.objects.filter(blog_type=blog_type)
     context["blog_type"]=blog_type
+    context["blog_types"] = BlogType.objects.all()
     return render(request,"blog/blogs_with_type.html",context)
