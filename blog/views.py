7aefada296c850a5812ddacab7d3d43110224c19
leayl <lea_yl@163.com>
2019-04-23 23:40:52 +0800

    add category count

--- blog/views.py
+++ blog/views.py
@@ -22,11 +22,20 @@ def get_blog_list_common_data(request,blogs_all_list):
         page_range.insert(0, 1)
     if page_range[-1] != paginator.num_pages:
         page_range.append(paginator.num_pages)
+    blog_dates =  Blog.objects.dates('created_time', 'day', order='DESC')
+    # 获取日期分类的数量
+    blog_dates_dict = {}
+    for blog_date in blog_dates:
+        blog_count =  Blog.objects.filter(created_time__year=blog_date.year,
+                                     created_time__month=blog_date.month,
+                                     created_time__day=blog_date.day,
+                                     ).count()
+        blog_dates_dict[blog_date]=blog_count
     context = {}
     context["page_of_blogs"] = page_of_blogs  # 将获取页面对象返回前端
     context["page_range"] = page_range  # 分页显示的页码范围
     context["blog_types"] = BlogType.objects.all()
-    context["blog_dates"] = Blog.objects.dates('created_time', 'day', order='DESC')
+    context["blog_dates"] =blog_dates_dict
     return context
 
 def blog_list(request):
